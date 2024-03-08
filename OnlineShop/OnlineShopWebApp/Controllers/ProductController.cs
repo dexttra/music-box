751b71e6eeec2059d8138a26944f9389e12bd0bd
d-laliev@mail.ru <d-laliev@mail.ru>
2024-03-08 12:05:26 +0300

    Сделал задание 2_2

---
+++ OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
+using System.Diagnostics.Eventing.Reader;
+using System.Security.Cryptography.X509Certificates;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public string Index(int id)
+        {       
+            var p = ProductsList.Products.Single(p => p.Id == id);
+            return p.ToString();
+        }
+    }
+}
