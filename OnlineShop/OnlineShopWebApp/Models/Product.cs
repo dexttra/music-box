a3ee3a0b2bfa13bba084f543e1cd8ecdc330a4ec
d-laliev@mail.ru <d-laliev@mail.ru>
2024-03-08 10:33:50 +0300

    Создал класс Product

---
+++ OnlineShop/OnlineShopWebApp/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace OnlineShopWebApp.Models
+{
+    public class Product
+    {
+        private static int UniqId;
+        public int Id { get; }
+        public string Name { get; set; }
+        public decimal Cost { get; set; }
+        public string Description { get; set; }
+
+        public Product(string name, decimal cost, string description)
+        {
+            Id = UniqId++;
+            Name = name;
+            Cost = cost;
+            Description = description;
+        }
+    }
+}
