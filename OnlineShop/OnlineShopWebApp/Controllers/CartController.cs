fcacadad5c125d600204f2bb622445f9186dade2
d-laliev@mail.ru <d-laliev@mail.ru>
2024-03-14 15:19:57 +0300

    Сделал красивое отображение товара

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CartController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
