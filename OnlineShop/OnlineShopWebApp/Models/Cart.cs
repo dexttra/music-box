5a71751dbce9559f01195e777101b099a3018c24
d-laliev@mail.ru <d-laliev@mail.ru>
2024-03-14 21:50:30 +0300

    Изменил несколько файлов

---
+++ OnlineShop/OnlineShopWebApp/Models/Cart.cs
@@ -0,0 +1,18 @@
+﻿namespace OnlineShopWebApp.Models
+{
+    public class Cart
+    {
+        public readonly int UserId;
+        public List<Product> ProductsList {  get; set; } = new List<Product>();
+
+        public Cart(int id) 
+        {
+            UserId = id;
+        }
+
+        public void AddProduct(Product product)
+        {
+            ProductsList.Add(product);
+        }
+    }
+}
---
+++ OnlineShop/OnlineShopWebApp/Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace OnlineShopWebApp.Models
+{
+    public class User
+    {
+        private static int uniqId;
+        public int Id { get; }
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public string Email { get; private set; }
+
+        public User(string name, string surname, string email)
+        {
+            Id = uniqId++;
+            Name = name;
+            Surname = surname;
+            Email = email;
+        }
+    }
+}
