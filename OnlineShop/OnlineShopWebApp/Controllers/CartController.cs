479c8a18a58c541585043c020c74e8acc7814551
d-laliev@mail.ru <d-laliev@mail.ru>
2024-03-15 11:03:39 +0300

    Исправил замечания

--- OnlineShop/OnlineShopWebApp/Controllers/CartController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/CartController.cs
@@ -7,14 +7,14 @@ namespace OnlineShopWebApp.Controllers
     {
         public IActionResult Index()
         {
-            var cart = CartsStorage.TryGetByUserId(1);
+            var cart = CartsStorage.TryGetByUserId(Constants.UserId);
             return View(cart);
         }
 
         public IActionResult Add(int productId)
         {
             var product = ProductsStorage.TryGetById(productId);
-            CartsStorage.Add(product, 1);
+            CartsStorage.Add(product, Constants.UserId);
             return RedirectToAction("Index");          
         }
     }
---
+++ OnlineShop/OnlineShopWebApp/Models/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace OnlineShopWebApp.Models
+{
+	public static class Constants
+	{
+		public static int UserId = 1;
+	}
+}
