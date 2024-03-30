2a173e2e623bc78ab7b848bd0750adc49e7b5807
d-laliev@mail.ru <d-laliev@mail.ru>
2024-03-30 17:20:41 +0300

    Задание 5_2

---
+++ OnlineShop/OnlineShopWebApp/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+	public class LoginController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult SignIn(string email, string password)
+		{
+			return View("Index");
+		}
+
+		public IActionResult SignUp()
+		{
+			return View();
+		}
+	}
+}
