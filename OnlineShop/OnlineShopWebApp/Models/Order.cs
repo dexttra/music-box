ff1b089ce20809227b0a1484675b5d76d8ac68ad
d-laliev@mail.ru <d-laliev@mail.ru>
2024-04-08 01:27:50 +0300

    Сделал задание 6_2

--- OnlineShop/OnlineShopWebApp/Models/Order.cs
+++ OnlineShop/OnlineShopWebApp/Models/Order.cs
@@ -1,26 +1,36 @@
-﻿namespace OnlineShopWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopWebApp.Models
 {
 	public class Order
 	{
 		public int Id { get; }
 		private static int uniqId = 0;
-		public string Name { get; set; }
-		public string Surname { get; set; }
-		public string Email { get; set; }
-		public string City { get; set; }
-		public string Street { get; set; } 
-		public string StreetNumber { get; set; }
+        [Required(ErrorMessage = "Заполните поле")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Заполните поле")]
+        public string Surname { get; set; }
+
+		[EmailAddress]
+        [Required(ErrorMessage = "Заполните поле")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Заполните поле")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Заполните поле")]
+        public string City { get; set; }
+        [Required(ErrorMessage = "Заполните поле")]
+        public string Street { get; set; } 
 		public Cart Cart { get; set; }
 
-		public Order(string name, string surname, string email, string city, string street, string streetNumber, Cart cart)
+		public Order(string name, string surname, string email, string phone, string city, string street, Cart cart)
 		{
 			Id = uniqId++;
 			Name = name;
 			Surname = surname;
 			Email = email;
+			Phone = phone;
 			City = city;
 			Street = street;
-			StreetNumber = streetNumber;
 			Cart = cart;
 		}
 
